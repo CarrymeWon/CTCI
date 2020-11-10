d2c917450d04d655e6a5dbf2a1803e277c00c841
XWang <xiaoyu.wang@ika.rwth-aachen.de>
2020-11-10 18:25:15 +0100

    1.4 with optimized solution

--- Vscode/1.4.cpp
+++ Vscode/1.4.cpp
@@ -2,8 +2,8 @@
 #include<string>
 
 using namespace std;
-/*Palindrome is case non-sensitive*/
-bool palinPermutation(string s1) {
+/*Palindrome isn't case sensitive*/
+bool palinPermutation1(string s1) {
 	int len = s1.length();
 	int a[26];
 	memset(a, 0, sizeof(a));
@@ -35,10 +35,47 @@ bool palinPermutation(string s1) {
 	}
 	return 1;
 }
+/*Optimize method: check if it is odd counts as it goes along*/
+
+bool palinPermutation2(string s1) {
+	int len = s1.length();
+	int mark = 0;
+	int a[26];
+	memset(a, 0, sizeof(a));
+	for (int i = 0; i < len; i++) {
+		int val = (int)s1[i];
+		/* lowercased letters ascii are from 97 to 122,
+		uppercased letters ascii are from 65 to 90*/
+		if (val >= 65 && val < 90) {
+			val -= 'A';
+			a[val] = (a[val] + 1) % 2;
+			if (a[val] != 0)
+				mark++;
+			else
+				mark--;
+		}
+		if (val >= 97 && val <= 122) {
+			val -= 'a';
+			a[val] = (a[val] + 1) % 2;
+			if (a[val] != 0)
+				mark++;
+			else
+				mark--;
+		}
+
+	}
+
+	if (mark == 0 || mark == 1)
+		return 1;
+	else
+		return 0;
+
+
+}
 
 int main(){
 	string s1;
 	getline(cin, s1);
-	cout << palinPermutation(s1) << endl;
+	cout << palinPermutation2(s1) << endl;
 
 }
